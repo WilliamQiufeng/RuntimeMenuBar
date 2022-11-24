98352ab59f9c695cc9ec81e865ae1a36abd9398b
QiuFeng54321 <32996262+QiuFeng54321@users.noreply.github.com>
2022-11-24 21:53:04 +0800

    Make shortcut usable

--- Runtime/RMB/UI/MenuBar.cs
+++ Runtime/RMB/UI/MenuBar.cs
@@ -11,9 +11,10 @@ namespace RMB.UI
         public GameObject rootMenuItemContainer;
         public MenuBarSO menuBarSO;
         public GameObject rootMenuItemPrefab;
-        public readonly Stack<MenuItem> Path = new();
-        internal readonly List<MenuItem> RootMenuItems = new();
-        internal bool IsFocused;
+        private readonly Stack<MenuItem> _path = new();
+        private readonly List<MenuItem> _rootMenuItems = new();
+        internal readonly Dictionary<Event, MenuItem> Shortcuts = new();
+        private bool _isFocused;
         internal bool IsMenuOpen;
 
         private void Start()
@@ -23,49 +24,81 @@ namespace RMB.UI
 
         private void Update()
         {
-            if (IsMenuOpen && Input.GetMouseButtonDown(0) && !IsFocused) HideAll();
+            // Hide the menu if the user clicks outside the menu
+            if (IsMenuOpen && Input.GetMouseButtonDown(0) && !_isFocused) HideAll();
         }
 
+        private void OnGUI()
+        {
+            // Process any shortcut events
+            foreach (var (shortcutEvent, menuItem) in Shortcuts)
+                if (Event.current.Equals(shortcutEvent))
+                    menuItem.OnClick();
+        }
+
+        // Handle focused
         public void OnPointerEnter(PointerEventData eventData)
         {
-            IsFocused = true;
+            _isFocused = true;
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            IsFocused = false;
+            _isFocused = false;
         }
 
         public void GotoItem(MenuItem item)
         {
-            if (Path.Count > 0 && Path.Peek() == item) return;
-            while (Path.Count > 0 && !Path.Peek().SubItems.Contains(item))
+            // Current item is already expanded: skip
+            if (_path.Count > 0 && _path.Peek() == item) return;
+            // Current item not contained in the top of the path: pop and contract
+            while (_path.Count > 0 && !_path.Peek().SubItems.Contains(item))
             {
-                var popped = Path.Pop();
+                var popped = _path.Pop();
                 popped.ContainerEnabled = false;
             }
 
-            Path.Push(item);
+            // Push the item to the path. This will not expand its container
+            _path.Push(item);
+        }
+
+        public void GotoItemRecursive(MenuItem item)
+        {
+            HideAll(); // Hide (essentially clearing the path)
+            GotoItemRecursiveInternal(item);
+        }
+
+        internal void GotoItemRecursiveInternal(MenuItem item)
+        {
+            // Start from root
+            if (item.ParentMenuItem != null) GotoItemRecursiveInternal(item.ParentMenuItem);
+            // Expands upwards
+            GotoItem(item);
+            // This expands the container
+            item.ContainerEnabled = true;
         }
 
         public void HideAll()
         {
-            while (Path.Count > 0)
+            // Contract and pop all items in path
+            while (_path.Count > 0)
             {
-                var popped = Path.Pop();
+                var popped = _path.Pop();
                 popped.ContainerEnabled = false;
                 Debug.Log($"Hide {popped.itemName.text}");
             }
 
-            IsMenuOpen = false;
+            IsMenuOpen = false; // Set state
         }
 
         public void UpdateInfo()
         {
-            RootMenuItems.Clear();
+            // Clear
+            _rootMenuItems.Clear();
+            Shortcuts.Clear();
             if (menuBarSO == null)
             {
-                RootMenuItems.AddRange(rootMenuItemContainer.transform.GetChildrenComponent<MenuItem>());
+                _rootMenuItems.AddRange(rootMenuItemContainer.transform.GetChildrenComponent<MenuItem>());
             }
             else
             {
@@ -75,16 +108,22 @@ namespace RMB.UI
                     var instance = Instantiate(rootMenuItemPrefab, transform);
                     var menuItem = instance.GetComponent<MenuItem>();
                     menuItem.menuItemSO = rootMenuItem;
-                    RootMenuItems.Add(menuItem);
+                    _rootMenuItems.Add(menuItem);
                 }
             }
 
             UpdateItemsInfo();
+            RegisterShortcutEvents();
+        }
+
+        internal void RegisterShortcutEvents()
+        {
+            foreach (var subItem in _rootMenuItems) subItem.RegisterShortcutEvents();
         }
 
         private void UpdateItemsInfo()
         {
-            foreach (var rootMenuItem in RootMenuItems)
+            foreach (var rootMenuItem in _rootMenuItems)
             {
                 rootMenuItem.UpdateInfo();
                 rootMenuItem.MenuBar = this;
--- Runtime/RMB/UI/MenuItem.cs
+++ Runtime/RMB/UI/MenuItem.cs
@@ -12,6 +12,8 @@ namespace RMB.UI
 {
     public class MenuItem : MonoBehaviour, IPointerEnterHandler
     {
+        public string inputBinding; // https://docs.unity3d.com/ScriptReference/Event.KeyboardEvent.html
+        public bool closeOnClick;
         public MenuItemSO menuItemSO;
         public TMP_Text itemName;
         public TMP_Text shortcutText;
@@ -20,12 +22,10 @@ namespace RMB.UI
         public GameObject rootMenuItemPrefab;
         public GameObject menuSubItemPrefab;
         public UnityEvent additionalOnClick;
-        public bool closeOnClick;
-        public string inputBinding; // https://docs.unity3d.com/ScriptReference/Event.KeyboardEvent.html
         [NonSerialized] internal readonly List<MenuItem> SubItems = new();
+        private Event _shortcutEvent;
         internal MenuBar MenuBar;
         internal MenuItem ParentMenuItem;
-        internal Event ShortcutEvent;
 
         public bool ContainerEnabled
         {
@@ -33,84 +33,100 @@ namespace RMB.UI
             set => container.SetActive(value);
         }
 
-        public bool IsRootMenuItem => ParentMenuItem == null;
+        private bool IsRootMenuItem => ParentMenuItem == null;
 
-
-        private void OnGUI()
-        {
-            if (!string.IsNullOrEmpty(inputBinding))
-                if (Event.current.Equals(ShortcutEvent))
-                    OnClick();
-        }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
+            // Skip when menu is not open
             if (!MenuBar.IsMenuOpen) return;
+            // If any of its children is ray-casted, skip and leave this event to the ultimate child to process.
+            // since that's the target the user is hitting
             if (eventData.hovered.Any(h =>
                     h.TryGetComponent(out MenuItem menuItem) && SubItems.Contains(menuItem))) return;
+            // Go to the specific item. It shouldn't need to be recursive since it should be one of the open path
             MenuBar.GotoItem(this);
+            // Enable the container.
             ContainerEnabled = true;
-            // Debug.Log(string.Join(", ", MenuBar.Path.Select(s => s.itemName.text)));
         }
 
 
         public void OnClick(bool fromGUI = false)
         {
-            if (fromGUI && (container.transform.childCount > 0 ||
-                            (menuItemSO != null && menuItemSO.elements.Count > 0)))
+            if (fromGUI)
             {
-                ContainerEnabled = !ContainerEnabled;
+                ContainerEnabled = !ContainerEnabled; // Toggle container
                 if (ContainerEnabled)
                 {
-                    MenuBar.IsMenuOpen = true;
-                    MenuBar.Path.Push(this);
+                    MenuBar.IsMenuOpen = true; // Open Menu if now enabled
+                    MenuBar.GotoItem(this);
                 }
                 else if (IsRootMenuItem)
                 {
-                    MenuBar.IsMenuOpen = false;
+                    MenuBar.IsMenuOpen = false; // Close Menu
                     MenuBar.HideAll();
                 }
             }
 
-            Debug.Log($"Click: {itemName.text}");
+            // Invoke OnClick events
             additionalOnClick?.Invoke();
             if (menuItemSO != null) menuItemSO.onClickEvent?.Invoke();
+            // Hide the menu if the menu item specifies a close when clicked
             if (closeOnClick) MenuBar.HideAll();
+            // From this point the logic is dependent on unchanged value of ContainerEnabled, so skip if the call
+            // is from GUI since it toggles the state of ContainerEnabled
+            if (fromGUI) return;
+            // This comes from a shortcut event. Triggering this item when it's open already will close it
+            if (ContainerEnabled) MenuBar.HideAll();
+            // If it specifies that it will not close when clicked on, we should expand the panel to show this menu item.
+            // If it closes when clicked on, it doesn't bring up the menu (e.g. Ctrl + S for save)
+            else if (!closeOnClick && !ContainerEnabled) MenuBar.GotoItemRecursive(this);
         }
 
         public void UpdateInfo()
         {
+            // Close the container
             ContainerEnabled = false;
+            // Clear all sub-items
             SubItems.Clear();
             if (menuItemSO == null)
             {
+                // Add all existing sub-items in its children
                 SubItems.AddRange(container.transform.GetChildrenComponent<MenuItem>());
-                if (iconContainer != null) iconContainer.SetActive(SubItems.Count != 0);
-                if (!string.IsNullOrEmpty(inputBinding)) ShortcutEvent = Event.KeyboardEvent(inputBinding);
+                // Initialize shortcut event from inputBinding
+                if (!string.IsNullOrEmpty(inputBinding)) _shortcutEvent = Event.KeyboardEvent(inputBinding);
             }
             else
             {
+                // Set name from SO
                 itemName.text = menuItemSO.menuName;
-                if (shortcutText != null) ShortcutEvent = Event.KeyboardEvent(menuItemSO.shortcutEvent);
+                // Initialize shortcut event from SO
+                if (shortcutText != null) _shortcutEvent = Event.KeyboardEvent(menuItemSO.shortcutEvent);
 
+                // Clear all container's children
                 container.transform.ClearChildren();
-
-                if (iconContainer != null) iconContainer.SetActive(menuItemSO.elements.Count != 0);
                 foreach (var itemSO in menuItemSO.elements)
                 {
+                    // Instantiate its sub-items
                     var instance = Instantiate(itemSO.menuItemType is MenuItemType.Child
                         ? menuSubItemPrefab
                         : rootMenuItemPrefab, container.transform);
                     var menuItem = instance.GetComponent<MenuItem>();
                     menuItem.menuItemSO = itemSO;
-                    SubItems.Add(menuItem);
+                    SubItems.Add(menuItem); // Add to list
                 }
             }
 
-            if (shortcutText != null) shortcutText.text = ShortcutEvent == null ? "" : ShortcutEvent.ToKeybindString();
+            // Enable or disable the expansion icon depending on whether it has sub-items or not
+            if (iconContainer != null) iconContainer.SetActive(SubItems.Count != 0);
+
+            // Set shortcut text
+            if (shortcutText != null)
+                shortcutText.text = _shortcutEvent == null ? "" : _shortcutEvent.ToKeybindString();
             foreach (var subItem in SubItems)
             {
                 Debug.Log($"Update Info: {subItem.itemName.text}");
+                // Recurse on its children
                 subItem.UpdateInfo();
             }
         }
@@ -119,11 +135,18 @@ namespace RMB.UI
         {
             foreach (var subItem in SubItems)
             {
-                Debug.Log($"Update: {subItem.itemName.text}");
+                // Update sub-items' parent
                 subItem.ParentMenuItem = this;
                 subItem.MenuBar = MenuBar;
                 subItem.UpdateSubItemsParent();
             }
+        }
+
+        internal void RegisterShortcutEvents()
+        {
+            // Register shortcut events to the dictionary in menubar.
+            if (_shortcutEvent != null) MenuBar.Shortcuts.Add(_shortcutEvent, this);
+            foreach (var subItem in SubItems) subItem.RegisterShortcutEvents();
         }
     }
 }
